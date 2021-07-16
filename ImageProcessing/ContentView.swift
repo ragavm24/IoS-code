31e97d3598286017efb4d4fe52f4317ebc717693
ragavm24 <ragav1997@gmail.com>
2021-07-16 01:31:10 -0700

    Initial Commit

---
+++ ImageProcessing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageProcessing
+//
+//  Created by Ragav on 7/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageProcessing/ImageProcessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageProcessingApp.swift
+//  ImageProcessing
+//
+//  Created by Ragav on 7/16/21.
+//
+
+import SwiftUI
+
+@main
+struct ImageProcessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
